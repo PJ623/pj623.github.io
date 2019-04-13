124d811b7e2278e92d321b120b156473be0699f0
PJ623 <jardiolin.paolo@gmail.com>
2019-04-13 14:35:09 -0700

    simplified bunch of code

--- js/main.js
+++ js/main.js
@@ -56,66 +56,11 @@ projectCollection.addAll(projects.map(function (p) { return makeProjectElement(p
 // Makes images viewable in modal.
 function makeClickableImage(ele) {
     ele.onclick = myModal.show.bind(null, ele);
-    ele.style.cursor = "pointer";
 }
 
-// Timing style for desktop CSS animations
-if (window.matchMedia("(min-width: 768px)").matches) {
-    setTimeout(() => {
-        const sectionLinks = document.querySelector("section a");
-        if (sectionLinks.length > 1) {
-            Array.from(sectionLinks).forEach((a) => {
-                a.style.cursor = "pointer";
-            });
-        } else {
-            sectionLinks.style.cursor = "pointer";
-        }
-    }, 3500);
-
-    setTimeout(() => {
-        const collectionImages = projectCollection.getElementsByTagName("img");
-        if (collectionImages.length > 1) {
-            Array.from(collectionImages).forEach(makeClickableImage);
-        } else {
-            makeClickableImage(collectionImages);
-        }
-    }, 3600);
-
-    setTimeout(() => {
-        const collectionLinks = projectCollection.getElementsByTagName("a");
-        if (collectionLinks.length > 1) {
-            Array.from(collectionLinks).forEach((a) => {
-                a.style.cursor = "pointer";
-            });
-        } else {
-            collectionLinks.style.cursor = "pointer";
-        }
-    }, 3600);
+const collectionImages = projectCollection.getElementsByTagName("img");
+if (collectionImages.length > 1) {
+    Array.from(collectionImages).forEach(makeClickableImage);
 } else {
-    console.log("mobile");
-    // clean up redundant code later
-    const sectionLinks = document.querySelector("section a");
-    if (sectionLinks.length > 1) {
-        Array.from(sectionLinks).forEach((a) => {
-            a.style.cursor = "pointer";
-        });
-    } else {
-        sectionLinks.style.cursor = "pointer";
-    }
-
-    const collectionLinks = projectCollection.getElementsByTagName("a");
-    if (collectionLinks.length > 1) {
-        Array.from(collectionLinks).forEach((a) => {
-            a.style.cursor = "pointer";
-        });
-    } else {
-        collectionLinks.style.cursor = "pointer";
-    }
-
-    const collectionImages = projectCollection.getElementsByTagName("img");
-    if (collectionImages.length > 1) {
-        Array.from(collectionImages).forEach(makeClickableImage);
-    } else {
-        makeClickableImage(collectionImages);
-    }
+    makeClickableImage(collectionImages);
 }
